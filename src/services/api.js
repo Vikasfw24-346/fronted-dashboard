d2872dd3e5f07ef4bfa3ca3e9551c560889984d8
Vikas Tiwari <119485027+Vikasfw24-346@users.noreply.github.com>
2026-01-30 12:16:33 -0800

    api_urls added here:

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
-
+const API_URLS = import.meta.env.VITE_API_URLS;
 export const fetchInsights = async (filters = {}) => {
-  const { data } = await axios.get(`${API_URL}/api/insights`, {
+  const { data } = await axios.get(`${API_URL || API_URLS}/api/insights`, {
     params: filters,
   });
   return data;
