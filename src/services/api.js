c1c9a070b5c3bc9a6892fbb5ace63326a88989f5
Vikas Tiwari <119485027+Vikasfw24-346@users.noreply.github.com>
2026-01-31 00:39:14 -0800

    Update API URL to use render service

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+const API_URL_RENDER = "https://backend-dashboard-xwpb.onrender.com";
 const API_URL = import.meta.env.VITE_API_URL;
 const API_URLS = import.meta.env.VITE_API_URLS;
 export const fetchInsights = async (filters = {}) => {
-  const { data } = await axios.get(`${API_URL || API_URLS}/api/insights`, {
+  const { data } = await axios.get(`${API_URL_RENDER || API_URL || API_URLS}/api/insights`, {
     params: filters,
   });
   return data;
